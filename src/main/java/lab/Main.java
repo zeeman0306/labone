8c03ff53a72085e7f041ba9d0348f792877a598f
zeeman0306 <zeeman0306@gmail.com>
2018-04-03 16:53:08 -0400

    updated

--- src/main/java/lab/Main.java
+++ src/main/java/lab/Main.java
@@ -3,5 +3,6 @@ package lab;
 public class Main {
     public static void main(String[] args) {
         System.out.println("it works!");
+        System.out.println("FOr the super negroes!");
     }
 }
